2b65eb2770c9c7a0f908a989bad987b3cd79d2ee
Guilherme Fontebasso <guitavaresgames@gmail.com>
2022-07-16 13:39:33 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Computers");
-		System.out.println(obj);
+		Department obj = new Department(2, "Computers");
+		
+		Seller seller = new Seller(06, "Guilherme", "gui@gmail.com", new Date(), 5000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
