6d7586c5e59ebd066e7e90724bbf462b87752292
Guilherme Fontebasso <guitavaresgames@gmail.com>
2022-07-16 13:29:45 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Computers");
+		System.out.println(obj);
+
+	}
+
+}
